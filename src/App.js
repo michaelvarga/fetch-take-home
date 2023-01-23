46f897ab60d38efbe2ffd53750022467713d76e3
Mike Varga <mikevarga07@gmail.com>
2023-01-23 15:53:57 -0600

    created the skeleton of the form without any style or GET/POST request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    occupation: "",
+    state: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label for="occupation">Occupation</label>
+          <input type="text" name="occupation" id="occupation" value={formData.occupation} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label for="state">State</label>
+          <input type="text" name="state" id="state" value={formData.state} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} required/>
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
